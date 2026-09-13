8489b1e0ed637f5e59d65e42e2477b0ee7ab142c
nebrasacademy3-lab <nebrasacademy3@gmail.com>
2026-09-14 00:35:06 +0300

    ان

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const contentSecurityPolicy = [
   "img-src 'self' data: blob: https:",
   "font-src 'self' data:",
   "media-src 'self' blob:",
-  "connect-src 'self' https://api.tap.company https://*.tap.company",
+  "connect-src 'self' https://api.tap.company https://*.tap.company https://*.t3.storageapi.dev",
   "frame-src 'self' https://*.tap.company",
   "worker-src 'self' blob:",
   "manifest-src 'self'",
